183ee9eb2b877803d7543251818784a8c4aca13a
MarianaSiano <marianasianop@gmail.com>
2025-05-06 17:28:02 -0300

    Arquivo 'App.js' parte atualizado

--- rbac-seguranca/src/App.js
+++ rbac-seguranca/src/App.js
@@ -3,3 +3,6 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './Components/UI/ProtectedRoute';
 import Login from './Components/Auth/Login';
+import BoardPanel from './Components/BoardPanel';
+import TeachersPanel from './Components/TeachersPanel';
+import StudentPanel from './Components/StudentPanel';
