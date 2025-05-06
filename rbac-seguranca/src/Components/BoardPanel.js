90c523b2068fdd0d8b7908a9f95a2877956a3a73
MarianaSiano <marianasianop@gmail.com>
2025-05-06 14:51:41 -0300

    Arquivo 'BoardPanel' concluido

--- rbac-seguranca/src/Components/BoardPanel.js
+++ rbac-seguranca/src/Components/BoardPanel.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
+
+const BoardPanel = () => {
+    const { usuario } = useAuth();
+
+    return (
+        <div className='panel'>
+            <h2>Painel da Diretoria</h2>
+            <p>Bem-Vindo(a), {usuario?.nome}!</p>
+            
+            <div className='recursos'>
+                <button>Gerenciar Professores</button>
+                <button>Gerenciar Alunos</button>
+                <button>Emitir Relatórios</button>
+                <button>Configurações da Escola</button>
+            </div>
+        </div>
+    );
+};
+
+export default BoardPanel;
