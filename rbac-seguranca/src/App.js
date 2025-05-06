dfd6f858ef619871950f73f9ce290e620a7fc00c
MarianaSiano <marianasianop@gmail.com>
2025-05-06 19:09:52 -0300

    App.js concluido

--- rbac-seguranca/src/App.js
+++ rbac-seguranca/src/App.js
@@ -6,3 +6,46 @@ import Login from './Components/Auth/Login';
 import BoardPanel from './Components/BoardPanel';
 import TeachersPanel from './Components/TeachersPanel';
 import StudentPanel from './Components/StudentPanel';
+import Navbar from './Components/UI/Navbar';
+import Unauthorizad from './Components/Unauthorized';
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <AuthProvider>
+                <div className='App'>
+                    <Navbar />
+                    <Switch>
+                        <Route path='/login' component={Login} />
+                        <Route path='/nao-autorizado' component={Unauthorizad} />
+
+                        <ProtectedRoute 
+                        path='/diretoria'
+                        component={BoardPanel}
+                        permissao='gerenciar_usuarios'
+                        />
+
+                        <ProtectedRoute 
+                        path='/professor'
+                        component={TeachersPanel}
+                        permissao='lancar_notas'
+                        />
+
+                        <ProtectedRoute 
+                        path='/aluno'
+                        component={StudentPanel}
+                        permissao='visualizar_notas'
+                        />
+
+                        <Route exact path='/'>
+                            <Redirect to='/login' />
+                        </Route>
+                    </Switch>
+                </div>
+            </AuthProvider>
+        </Router>
+    );
+};
+
+export default App;
