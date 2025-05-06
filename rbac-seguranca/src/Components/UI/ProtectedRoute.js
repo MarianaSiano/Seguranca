3ff82ff708fd0b851773189deacddc60d8651449
MarianaSiano <marianasianop@gmail.com>
2025-05-06 13:21:00 -0300

    Arquivo 'ProtectedRoute.js' criado e terminado e o arquivo 'Lohi Login.js criado

---
+++ rbac-seguranca/src/Components/UI/ProtectedRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+
+const ProtectedRoute = ({ component: Component, permission, ...rest }) => {
+    const { isAuthenticated, can } = useAuth();
+
+    return (
+        <Route {...rest} render={(props) => {
+            if(!isAuthenticated())
+                return <Redirect to="/login" />;
+
+            if(permission && !can(permission))
+                return <Redirect to="Não autorizado" />
+
+            return <Component {...props} />
+        }}
+        />
+    );
+};
