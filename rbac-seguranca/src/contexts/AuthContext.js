f2fb5e778925f4a697c084d2d4c31ab6e8ed620b
MarianaSiano <marianasianop@gmail.com>
2025-05-06 12:34:30 -0300

    'AuthContext.js' terminado

---
+++ rbac-seguranca/src/contexts/AuthContext.js
@@ -0,0 +1,44 @@
+import React, { createContext, userState, useContext } from 'react';
+import { login as apiLogin, logout as apiLogout } from '../api/auth';
+import { hasPermission } from '../api/rbac';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const login = async (usermame, password) => {
+        try {
+            const userData = await apiLogin(username, password);
+            setUser(userData);
+            return userData;
+        } catch(error){
+            throw error;
+        }
+    };
+
+    const logout = async () => {
+        await apiLogin();
+        setUser(null);
+    };
+
+    const isAuthenticated = () => {
+        return user;
+    };
+
+    const can = (permission) => {
+        if(!user)
+            return false;
+
+        return hasPermission(user.roles, permission);
+    };
+
+    return (
+    <AuthContext.Provider value={{ user, login, logout, isAuthenticated, can }}>
+        {children}
+    </AuthContext.Provider>);
+};
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
