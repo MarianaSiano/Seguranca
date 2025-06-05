badf5437e9200c3a96ec00e1553f11b85a69fe3a
MarianaSiano <marianasianop@gmail.com>
2025-06-05 14:25:10 -0300

    Funcao auxiliar 'fatoresUnicos' concluido

--- diffie-hellman/Diffie-Hellman.js
+++ diffie-hellman/Diffie-Hellman.js
@@ -1,5 +1,22 @@
 const crypto = require('crypto');
 
+//Função auxiliar para obter fatores únicos de um número
+function fatoresUnicos(num)
+{
+    const fatores = new Set();
+    let divisor = 2;
+
+    while(num >= 2) {
+        if(num % divisor === 0) {
+            fatores.add(divisor);
+            num = num / divisor;
+        } else {
+            divisor++;
+        }
+    }
+    return [...fatores];
+}
+
 //Função para encontrar uma raiz primitiva módulo p
 function encontrarRaizPrimitiva(p)
 {
