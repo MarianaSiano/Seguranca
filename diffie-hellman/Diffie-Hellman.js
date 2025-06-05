302be40f0b2465f87b90d8ebfdbb0e1d781ae45e
MarianaSiano <marianasianop@gmail.com>
2025-06-05 14:41:54 -0300

    Teste de primalidade concluido

--- diffie-hellman/Diffie-Hellman.js
+++ diffie-hellman/Diffie-Hellman.js
@@ -36,6 +36,50 @@ function modPow(base, exponent, modulus)
     return result;
 }
 
+//Teste de primalidade Miller-Rabin
+function isPrimo(num, k = 10)
+{
+    if(num <= 1)
+        return false;
+
+    if(num <= 3)
+        return true;
+
+    if(num % 2 === 0 || num % 3 === 0)
+        return false;
+
+    let d = num - 1;
+    let s = 0;
+
+    while(d % 2 === 0) {
+        d /= 2;
+        s++;
+    }
+
+    const bases = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    for(let i = 0; i < Math.min(k, bases.length); i++) {
+        const a = bases[i];
+        if(a >= num)
+            continue;
+
+        let x = modPow(a, d, num);
+        if(x === 1 || x === num - 1)
+            continue;
+
+        let compisite = true;
+        for(let j = 0; j < s - 1; j++) {
+            x = modPow(x, 2, num);
+            if(x === num - 1) {
+                compisite = false;
+                break;
+            }
+        }
+        if(compisite)
+            return false;
+    }
+    return true;
+}
+
 //Função para encontrar uma raiz primitiva módulo p
 function encontrarRaizPrimitiva(p)
 {
